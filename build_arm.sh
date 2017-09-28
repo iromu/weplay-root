2276cd989420b0044bd358adc6927e509151e3d6
Iván Rodríguez Murillo <wantez@gmail.com>
2017-09-28 23:06:51 +0200

    trigger

--- build_arm.sh
+++ build_arm.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker build --no-cache -t iromu/weplay-common:arm      ./modules/common/arm
-docker build --no-cache -t iromu/weplay-io:arm          ./modules/io/arm
-docker build --no-cache -t iromu/weplay-compressor:arm  ./modules/compressor/arm
-docker build --no-cache -t iromu/weplay-emulator:arm    ./modules/emulator/arm
-docker build --no-cache -t iromu/weplay-web:arm         ./modules/web/arm
-docker build --no-cache -t iromu/weplay-rom:arm         ./modules/rom/arm
-docker build --no-cache -t iromu/weplay-discovery:arm   ./modules/discovery/arm
+docker build --no-cache -t iromu/weplay-common:arm      ./modules/common        -f ./modules/common/arm/Dockerfile
+docker build --no-cache -t iromu/weplay-io:arm          ./modules/io            -f ./modules/io/arm/Dockerfile
+docker build --no-cache -t iromu/weplay-compressor:arm  ./modules/compressor    -f ./modules/compressor/arm/Dockerfile
+docker build --no-cache -t iromu/weplay-emulator:arm    ./modules/emulator      -f ./modules/emulator/arm/Dockerfile
+docker build --no-cache -t iromu/weplay-web:arm         ./modules/web           -f ./modules/web/arm/Dockerfile
+docker build --no-cache -t iromu/weplay-rom:arm         ./modules/rom           -f ./modules/rom/arm/Dockerfile
+docker build --no-cache -t iromu/weplay-discovery:arm   ./modules/discovery     -f ./modules/discovery/arm/Dockerfile
