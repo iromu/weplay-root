08d054b7b23ed430e282f2bb7f558ebcefd6b560
Iván Rodríguez Murillo <wantez@gmail.com>
2017-09-29 16:08:19 +0200

    trigger on weplay-common

--- build_arm.sh
+++ build_arm.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker build --no-cache -t iromu/weplay-common:arm      ./modules/common        -f ./modules/common/Dockerfile_arm
-docker build --no-cache -t iromu/weplay-io:arm          ./modules/io            -f ./modules/io/Dockerfile_arm
-docker build --no-cache -t iromu/weplay-compressor:arm  ./modules/compressor    -f ./modules/compressor/Dockerfile_arm
-docker build --no-cache -t iromu/weplay-emulator:arm    ./modules/emulator      -f ./modules/emulator/Dockerfile_arm
-docker build --no-cache -t iromu/weplay-web:arm         ./modules/web           -f ./modules/web/Dockerfile_arm
-docker build --no-cache -t iromu/weplay-rom:arm         ./modules/rom           -f ./modules/rom/Dockerfile_arm
-docker build --no-cache -t iromu/weplay-discovery:arm   ./modules/discovery     -f ./modules/discovery/Dockerfile_arm
+docker build --no-cache -t iromu/weplay-common-arm:latest      ./modules/common        -f ./modules/common/Dockerfile_arm
+docker build --no-cache -t iromu/weplay-io-arm:latest          ./modules/io            -f ./modules/io/Dockerfile_arm
+docker build --no-cache -t iromu/weplay-compressor-arm:latest  ./modules/compressor    -f ./modules/compressor/Dockerfile_arm
+docker build --no-cache -t iromu/weplay-emulator-arm:latest    ./modules/emulator      -f ./modules/emulator/Dockerfile_arm
+docker build --no-cache -t iromu/weplay-web-arm:latest         ./modules/web           -f ./modules/web/Dockerfile_arm
+docker build --no-cache -t iromu/weplay-rom-arm:latest         ./modules/rom           -f ./modules/rom/Dockerfile_arm
+docker build --no-cache -t iromu/weplay-discovery-arm:latest   ./modules/discovery     -f ./modules/discovery/Dockerfile_arm
---
+++ push_arm.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+docker push iromu/weplay-common-arm:latest
+docker push iromu/weplay-io-arm:latest
+docker push iromu/weplay-compressor-arm:latest
+docker push iromu/weplay-emulator-arm:latest
+docker push iromu/weplay-web-arm:latest
+docker push iromu/weplay-rom-arm:latest
+docker push iromu/weplay-discovery-arm:latest
